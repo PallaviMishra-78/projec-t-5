63bbdd607c8ecd6fd0542d822a36eddeacef1b3d
saket1990 <saketkumarsunny@gmail.com>
2022-07-26 20:17:46 +0530

    four api created

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const {register}=require("../controllers/userController")
+const express = require("express")
+let router = express.Router()
+let controller = require("../controllers/userController")
+let {authentication,authorization_user} = require("../middleware/auth")
+
 
 router.post('/register', controller.createUser)
-
 router.post('/login', controller.login)
-
-
-
+router.get('/user/:userId/profile', authentication, authorization_user, controller.getUser)
+router.put("/user/:userId/profile",authentication, authorization_user, controller.update)
 
 module.exports = router;
