4c68bd7424df49bdcb9c3065dda2ceb75f9ba5e5
PallaviMishra-78 <pallavimishra77080@gmail.com>
2022-08-01 13:23:03 +0530

    m

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,37 +1,39 @@
 const mongoose = require("mongoose")
-
+const objectId = mongoose.Schema.Types.ObjectId
 
 const cartSchema = new mongoose.Schema(
     {
         userId: {
-            ObjectId: ref,
+            type: objectId,
+            ref: "User",
             required: true,
-            unique: true,
-            trim: true
+            trim: true,
+            unique: true
         },
         items: [{
             productId: {
-                ObjectId: ref,
+                type: objectId,
+                ref: "Product",
                 required: true,
-                trim: true
+                trim: true,
             },
             quantity: {
                 type: Number,
                 required: true,
-            },
+                min: 1
+            }
         }],
         totalPrice: {
             type: Number,
             required: true,
-            trim: true
+            trim: true,
         },
         totalItems: {
             type: Number,
             required: true,
-            trim: true
-        },
-
-    }, { timestamps: true })
+            trim: true,
+        }
+    }, { timestamps: true }
+)
 
 module.exports = mongoose.model("Cart", cartSchema)
-
